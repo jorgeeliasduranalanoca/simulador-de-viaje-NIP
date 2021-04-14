772a938a0dcf382bbbcbf11559ef361dff83d201
jorgeduran1996 <jorgeduran1596@gmail.com>
2021-04-14 01:20:08 -0300

    AgregadoDeAJAX

--- send.js
+++ send.js
@@ -1,11 +1,11 @@
 var URLPOST ="https://jsonplaceholder.typicode.com/posts"
-var send = JSON.stringify(InfoSend)
+
             
 var infopost = InfoSend;
    $(".send").click(()  => {
        $.post(URLPOST,infopost,(respuesta,estado) => {
            if(estado === "success"){
-               alert("los datos del viaje fueron cargados con exito "+respuesta);
+               alert("los datos del viaje fueron enviados con exito "+respuesta);
                
            }
        });
